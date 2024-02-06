44e937f64664bf36dc70188ae0192da7c6a133ec
Your Name <you@example.com>
2024-02-06 14:15:56 -0800

    fixing

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -19,7 +19,7 @@ app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     '''Matching languages'''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
