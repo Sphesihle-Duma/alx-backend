4fa082d47fe2d55fff69f8d49237bdbe2f8d11f6
Your Name <you@example.com>
2024-02-06 11:42:39 -0800

    rendering the index template

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''Flask app module'''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    '''index view function'''
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
